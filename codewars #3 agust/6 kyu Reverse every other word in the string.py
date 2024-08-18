4939e23600dedc738e8fb5669490182523f1e43c
interesting12 <giorgishavadze09@gmail.com>
2024-08-18 16:19:01 +0400

    second commit

---
+++ codewars #3 agust/6 kyu Reverse every other word in the string.py
@@ -0,0 +1,7 @@
+def reverse_alternate(st):
+    word = st.split()
+    for i in range(len(word)):
+        i = word[::-1]
+        
+    reverse = " ".join(word)
+    return reverse
---
+++ codewars #3 agust/7 kyu multiply_all.py
@@ -0,0 +1,10 @@
+#https://www.codewars.com/kata/586909e4c66d18dd1800009b
+
+def multiply_all (ints):
+
+    def multiplyAll(num):
+
+        return  [i * num for i in ints]
+            
+    return multiplyAll
+    
